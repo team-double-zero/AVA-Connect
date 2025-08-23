28964d03fa3d558ed8f8f592d3d89d8276ef11d4
ahnjh05141 <jaehyuns05141@gmail.com>
2025-08-23 14:53:17 +0900

    file manage

--- ava-debug.py
+++
@@ -1,3 +0,0 @@
-from soruce_codes import control_vm, fetch_img, fetch_vid
-
-fetch_img.request_queue("../AVA-Connect/q_img", debug= True)
--- ava-gen.py
+++
@@ -1,9 +0,0 @@
-from soruce_codes import control_vm, fetch_img, fetch_vid
-
-# 폴더 img_q 내의 json 파일들 순회 요청
-TUNNEL_OPEN = control_vm.vm_start()
-if TUNNEL_OPEN: 
-    fetch_img.request_queue("../AVA-Connect/q_img")
-
-print("[INFO] 예약된 작업이 끝났습니다. VM 을 종료합니다.")
-control_vm.vm_stop()
